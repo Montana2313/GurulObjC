d48f9d30610b3b0ad4507bd91caf6121e14e80de
ÖzgürElmasli <ozgur968@gmail.com>
2019-08-07 16:54:38 +0300

    Initial Commit

---
+++ GurulObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GurulObjC
+//
+//  Created by Mac on 7.08.2019.
+//  Copyright © 2019 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GurulObjC/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  GurulObjC
+//
+//  Created by Mac on 7.08.2019.
+//  Copyright © 2019 Mac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
